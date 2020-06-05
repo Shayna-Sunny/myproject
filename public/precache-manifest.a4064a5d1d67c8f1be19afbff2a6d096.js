e570b3aa8e9cfbf66e977d859df7ece2fc8dc444
Shayna-Sunny <65078262+Shayna-Sunny@users.noreply.github.com>
2020-06-05 18:14:29 +0530

    Add files via upload

---
+++ public/precache-manifest.a4064a5d1d67c8f1be19afbff2a6d096.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c57c0613202a7e4e57ea492232816d3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0687d10350836638ebb0",
+    "url": "/static/css/2.7f0a60d8.chunk.css"
+  },
+  {
+    "revision": "ea44211276abc1825222",
+    "url": "/static/css/main.17322a8a.chunk.css"
+  },
+  {
+    "revision": "0687d10350836638ebb0",
+    "url": "/static/js/2.df3e115e.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.df3e115e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea44211276abc1825222",
+    "url": "/static/js/main.fcc9d738.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
